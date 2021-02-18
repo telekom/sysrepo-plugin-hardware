32822649a08cc75dcfb6264c26f12788c694a441
Paul Dumitru <paul.dumitru@qualitance.com>
2021-02-18 10:33:09 +0200

    👌 IMPROVE use rapidJson library to parse to lshw output

--- src/ietf-hardware-plugin.cc
+++ src/ietf-hardware-plugin.cc
@@ -16,7 +16,10 @@
 // under the License.
 
 #include <sysrepo-cpp/Session.hpp>
+#include <rapidjson/document.h>
+#include <rapidjson/istreamwrapper.h>
 
+#include <fstream>
 #include <stdio.h>
 
 extern "C" {
@@ -24,6 +27,9 @@ extern "C" {
     int sr_plugin_init_cb(sr_session_ctx_t* session, void** private_data);
 }
 
+using libyang::S_Data_Node;
+
+using sysrepo::S_Callback;
 using sysrepo::S_Session;
 using sysrepo::S_Subscribe;
 using sysrepo::Session;
@@ -33,16 +39,22 @@ using sysrepo::Callback;
 struct HardwareModel {
     S_Subscribe subscription;
     S_Session sess;
+    static std::string moduleName;
 };
+
+std::string HardwareModel::moduleName = "ietf-hardware";
 
 void logMessage(sr_log_level_t log, std::string msg) {
     switch (log) {
         case SR_LL_ERR:
             SRP_LOG_ERRMSG(msg.c_str());
+            break;
         case SR_LL_WRN:
             SRP_LOG_WRNMSG(msg.c_str());
+            break;
         case SR_LL_INF:
             SRP_LOG_INFMSG(msg.c_str());
+            break;
         case SR_LL_DBG:
         default:
             SRP_LOG_DBGMSG(msg.c_str());
@@ -57,6 +69,30 @@ struct ConfigurationCallback : public Callback {
                         uint32_t /* request_id */,
                         void* /* private_ctx */) override {
         printCurrentConfig(session, module_name);
+        return SR_ERR_OK;
+    }
+
+    int oper_get_items(sysrepo::S_Session session,
+                       const char* module_name,
+                       const char* /* path */,
+                       const char* request_xpath,
+                       uint32_t /* request_id */,
+                       S_Data_Node& parent,
+                       void* /* private_data */) override {
+        using namespace rapidjson;
+        int rc = system("/usr/bin/lshw -json > /tmp/hardware_components.json");
+        if(rc == -1) {
+            logMessage(SR_LL_ERR, std::string("lshw command failed"));
+            return SR_ERR_CALLBACK_FAILED;
+        }
+
+        logMessage(SR_LL_DBG, std::string("lshw command returned:") + std::to_string(rc));
+
+        std::ifstream ifs("/tmp/hardware_components.json", std::ifstream::in);
+        IStreamWrapper isw(ifs);
+
+        Document doc;
+        doc.ParseStream(isw);
         return SR_ERR_OK;
     }
 
@@ -76,14 +112,27 @@ struct ConfigurationCallback : public Callback {
     }
 };
 
-HardwareModel theModel;
+static HardwareModel theModel;
 
 int sr_plugin_init_cb(sr_session_ctx_t* session, void** /*private_data*/)
 {
-    int rc = 0;
     theModel.sess = std::make_shared<Session>(session);
     theModel.subscription = std::make_shared<Subscribe>(theModel.sess);
-    return rc;
+    S_Callback callback = std::make_shared<ConfigurationCallback>();
+
+    std::string oper_xpath("/" + HardwareModel::moduleName + ":" + "hardware");
+    try {
+        theModel.subscription->module_change_subscribe(HardwareModel::moduleName.c_str(), callback,
+                nullptr, nullptr, 0, SR_SUBSCR_ENABLED | SR_SUBSCR_DONE_ONLY);
+        theModel.subscription->oper_get_items_subscribe(HardwareModel::moduleName.c_str(),
+                oper_xpath.c_str(), callback);
+    } catch (std::exception const& e) {
+        logMessage(SR_LL_ERR, std::string("sr_plugin_init_cb: ") + e.what());
+        theModel.subscription.reset();
+        return SR_ERR_OPERATION_FAILED;
+    }
+
+    return SR_ERR_OK;
 }
 
 void sr_plugin_cleanup_cb(sr_session_ctx_t* /*session*/, void* /*private_data*/)
